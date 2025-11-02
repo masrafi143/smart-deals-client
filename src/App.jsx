119d3dbee1a311fb1d42d85cfb9aba18c29dee4c
masrafi143 <masrafiofficial143@gmail.com>
2025-11-03 01:52:49 +0600

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Smart deals</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layout/RootLayout.jsx';
+import Home from './components/Home/Home.jsx';
+import AllProducts from './components/AllProducts/AllProducts.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home
+      },
+      {
+        path: 'allProducts',
+        Component: AllProducts
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>,
+)
