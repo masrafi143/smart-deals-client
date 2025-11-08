7fb85e164f0585954823b9b5e7cc78d7480360f2
masrafi143 <masrafiofficial143@gmail.com>
2025-11-08 21:57:22 +0600

    setup env

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7t8qEoSabOr_lQ8TYhXJ3awhvfMPQyuQ",
-  authDomain: "smart-deals-e7592.firebaseapp.com",
-  projectId: "smart-deals-e7592",
-  storageBucket: "smart-deals-e7592.firebasestorage.app",
-  messagingSenderId: "503722247552",
-  appId: "1:503722247552:web:78e3ed622f1350dcbced65"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
